ad4abf677e26a9a521b4e5d47022ee0df699ae60
kkmanuu <kipngenoemmanuel479@gmail.com>
2025-04-01 16:50:21 +0300

    Refactored App component: Added role-based private routes for admin pages and improved route structure.

--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,49 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
-import Register from './pages/Register'; // Added
+import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
 import CaseDetails from './pages/CaseDetails';
 import Report from './pages/Report';
 import EmergencyForm from './pages/EmergencyForm';
 import Home from './pages/Home';
-import PrivateRoute from './components/PrivateRoute';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import AdminFeedbackPage from './pages/AdminFeedbackPage';
+import AdminDashboard from './pages/AdminDashboard';
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
   return (
     <>
       <Navbar />
       <Routes>
+        {/* Public Routes */}
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} /> {/* Added */}
-        <Route path="/emergency" element={<EmergencyForm />} />
-        <Route
-          path="/"
-          element={
-            <PrivateRoute>
-              <Home />
-            </PrivateRoute>
-          }
-        />
+        <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/case/:caseId" element={<CaseDetails />} />
-        <Route
-          path="/case/:caseId"
-          element={
-            <PrivateRoute>
-              <CaseDetails />
-            </PrivateRoute>
-          }
-        />
         <Route path="/report" element={<Report />} />
-       
+        <Route path="/emergency" element={<EmergencyForm />} />
+
+        {/* Admin Only Routes */}
+        <Route 
+          path="/admin/feedback" 
+          element={
+            <PrivateRoute allowedRoles={['admin']}>
+              <AdminFeedbackPage />
+            </PrivateRoute>
+          } 
+        />
+        <Route 
+          path="/admin/dashboard" 
+          element={
+            <PrivateRoute allowedRoles={['admin']}>
+              <AdminDashboard />
+            </PrivateRoute>
+          } 
+        />
       </Routes>
       <Footer />
     </>
