7565d45987ccbabb65f66888ac984ae7d0d1036d
kkmanuu <kipngenoemmanuel479@gmail.com>
2025-04-09 19:53:28 +0300

    feat: add route for generating case report

--- server/routes/reportRoutes.js
+++ server/routes/reportRoutes.js
@@ -1,7 +1,7 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const reportController = require('../controllers/reportController');
+const reportController = require("../controllers/reportController");
 
-router.get('/cases', reportController.generateCaseReport);
+router.get("/cases", reportController.generateCaseReport);
 
 module.exports = router;
