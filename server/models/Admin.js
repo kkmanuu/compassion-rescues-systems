e0b3b4679ecd2ee58d72dbca7fb200cbb5c7967d
kkmanuu <kipngenoemmanuel479@gmail.com>
2025-05-05 15:52:38 +0300

    Fix SQL syntax error in Admin.create method

--- server/models/Admin.js
+++ server/models/Admin.js
@@ -8,6 +8,7 @@ class Admin {
     return rows[0];
   }
 
+  
   static async create(username, password, role = "staff") {
     const [result] = await pool.query(
       "INSERT INTO users (username, password, role, ) VALUES (?, ?, ?)",
